068f7f6ed9c66f9ae158cae8024417ef57b92b75
repl.it user <replituser@example.com>
2021-03-10 16:00:11 +0000

    Added code to demonstrate the use of data types

---
+++ basics/input/data_types.py
@@ -0,0 +1,10 @@
+print("What is your name human?")
+name = str(input())
+print("How old are you (in years)?")
+age = int(input())
+print("How tall are you (in meters)?")
+height = float(input())
+print("How much do you weigh (in kilograms)?")
+weight = int(input())
+print("{} you are {} years old and your BMI is {:.2f}.".format(name, age, weight/height**2))
+
--- basics/output/ascii_robot.py
+++ basics/output/ascii_robot.py
@@ -1,4 +1,3 @@
-#Display Beep
 print("########")
 print("# *  * #")
 print("# ---- #")
@@ -13,4 +12,11 @@ print("  #  #")
 print("  #  #")
 print("  #  #")
 print("###  ###")
+print("Please enter a character for the eye")
+y = input()
+print("Beep's expression is now as follows:")
+print("#########")
+print("#  {0}  {0}  # .".format (*y))
+print("#  ----  #")
+print("##########")
 
