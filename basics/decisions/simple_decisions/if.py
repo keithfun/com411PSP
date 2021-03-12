60ab43a47c06bf7b5f6ecd2b2f091521655e57fb
repl.it user <replituser@example.com>
2021-03-12 21:36:35 +0000

    Added code to demonstrate the use of an if statement

---
+++ basics/decisions/simple_decisions/if.py
@@ -0,0 +1,7 @@
+#Create a program that allows us to specify what type of book is Beep reading!
+print("What type of book is this")
+t =str(input())
+if (t == "adventure"):
+  print("I like adventure books!")
+print("Finished reading book!")
+
