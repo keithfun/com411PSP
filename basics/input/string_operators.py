3007f7580a634836b0d730757181d11d49bd99f6
repl.it user <replituser@example.com>
2021-03-12 16:41:23 +0000

    Added code to demonstrate the use of string operators

---
+++ basics/input/string_operators.py
@@ -0,0 +1,26 @@
+#Wish to create a program that allows us to imporve Beep health
+print("Please enter the number of lives.")
+l = int(input())
+
+print("Please enter the energy level.")
+e = int(input())
+
+print("Please enter the shield level.")
+s = int(input())
+
+print("Health has been set.")
+print("Lives:", "♥" * l)
+print("Energy:","♦" * e)
+print("Shield:","♦" *s)
+
+
+
+
+
+
+
+
+
+
+
+
