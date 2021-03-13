3b36c25d08a9311da51e22f12fd32843a2663bd9
repl.it user <replituser@example.com>
2021-03-13 01:02:24 +0000

    Added code to demonstrate the use of modullator

---
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -0,0 +1,8 @@
+#Experimenting with modulo operator
+print("Please enter a whole number.")
+x = int(input())
+if (x%2!=0):
+  print("Number {} is an odd number.".format (x))
+else:
+  print("Numbwer {} is an even number.".format (x))
+print("Thank you for using our program")
