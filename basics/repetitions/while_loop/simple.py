5dc6cb0bc28d57052ddebc2e0cc07c84315e10ff
repl.it user <replituser@example.com>
2021-03-13 19:00:58 +0000

    Added code to demonstrate the use of a simple while loop

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,9 @@
+#testing while loops
+#have to ask user for an input
+print("How many cables should I remove ?")
+cables = int(input())
+#applyng the while repetition to work out how many times the phrase needs to be repeated.
+x = 0
+while (cables>x):
+  x=x +1
+  print("Removed cable.")
