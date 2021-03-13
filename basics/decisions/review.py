95b7d2ffebf3dc3369929832f6ae1c86df23d75b
repl.it user <replituser@example.com>
2021-03-13 20:08:16 +0000

    Added code to demonstrate the use of a while loop with ascii

---
+++ basics/decisions/review.py
@@ -0,0 +1,19 @@
+#this is totally random
+#just showing off some skills that I've aquired
+#First code block should be mainly asking for user input
+print("Tell me something about you human ?")
+user_input = input()
+if(user_input =="handsome"):
+  print("You're handsome!")
+elif(user_input =="nice"):
+  print("You're very nice!")
+  print("Do you have a girlfriend ?")
+  gf_input = input()
+  print("Do you have a Maserati ?")
+  car_input = input()
+  if((gf_input == "no") and (car_input == "yes")):
+    print("You don't need one. You can always spin plates!")
+  else:
+    print("You need to work on bettering yourself!!!")
+else :
+  print("For a small ammount of 500THB you can get called Hansum Maaan in Pattaya ")
