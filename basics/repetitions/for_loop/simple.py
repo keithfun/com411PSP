f07662e8a17d7cc93d754820cb6e60947fd60118
repl.it user <replituser@example.com>
2021-03-17 15:29:32 +0000

    Added code to demonstrate use of a for loop statement

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,18 @@
+#ask user for input
+print("How many mountains should I display?")
+users_responce = int(input())
+mountain_dew = 0
+
+print("Displaying...")
+#for loop to display the number of mountain
+for mountain_dew in range (users_responce):
+  print()
+ 
+  print("    __")
+  print("   /  \\_")
+  print("  /^    \\")
+  print(" /  ^     \\_")
+  print("/ ^  ^      ^\\")
+
+print("Done!")
+
