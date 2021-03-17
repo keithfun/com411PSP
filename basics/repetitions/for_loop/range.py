9e18c42f0afa6d19c37c2dc8c394c6bea0b6abcd
repl.it user <replituser@example.com>
2021-03-17 16:17:58 +0000

    Added code to demonstrate the use of a simple for loop

---
+++ basics/repetitions/for_loop/range.py
@@ -0,0 +1,21 @@
+#ask user for input
+print("What level of brightness is required?")
+lvl_req = int(input())
+
+#summoning another variable that tracks user input
+lvl_current = 0 
+
+print("Adjusting brightness...")
+print()
+#applying the for loop for even number sequence
+
+for lvl_current in range(2,lvl_req+1,2):
+  print("Beep's birghtness level: ", end="")
+  print("**"*lvl_current)
+  print("Bop's brightness level: ", end="")
+  print("**"*lvl_current)
+  print()
+
+print("Adjustments complete!")
+
+
